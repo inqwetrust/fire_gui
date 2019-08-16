b7c2d602f80c3d059883e25807705db299431589
User <g60432042@gmail.com>
2019-08-16 10:11:55 +0800

    vv2

--- fire_gui_control.py
+++ fire_gui_control.py
@@ -1,5 +1,10 @@
 from firebase import firebase
-f = open("firebase_database_url.txt", "r")
+import os.path
+filename = 'firebase_database_url.txt'
+if os.path.isfile(filename):
+    f = open(filename, "r")
+else:
+    open(filename, 'a').close()
 firebase = firebase.FirebaseApplication(f.read(), None)
 result = firebase.get('/test/', None)
 print(result)
