d0b7aeb4455e7534e0374ce8e499dd7a5e31e8d3
User <g60432042@gmail.com>
2019-08-16 12:27:56 +0800

    vv2

--- fire_gui_control.py
+++ fire_gui_control.py
@@ -1,10 +1,13 @@
-import time
-import random
 from firebase import firebase
 import os.path
 import sys
 import socket
 import pyautogui
+# Code to check if left or right mouse buttons were pressed
+import win32api
+import time
+import win32gui
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -36,13 +39,36 @@ def monitor():
 
 
 def broadcast():
+    state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
+    state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
     while True:
-        data = {'x': random.randint(100,1000), 'y': random.randint(100,1000)}
-        result = firebase.get('/ip/', name=None)
-        for k, v in result.items():
-            print(k)
-            firebase.put(url='/ip/{}'.format(k), name='click', data=data)
-        time.sleep(1)
+        a = win32api.GetKeyState(0x01)
+        b = win32api.GetKeyState(0x02)
+
+        if a != state_left:  # Button state changed
+            state_left = a
+            print(a)
+            if a < 0:
+                print('Left Button Pressed')
+                flags, hcursor, (x, y) = win32gui.GetCursorInfo()
+                data = {'x': x, 'y': y}
+                result = firebase.get('/ip/', name=None)
+                for k, v in result.items():
+                    print(k)
+                    firebase.put(url='/ip/{}'.format(k), name='click', data=data)
+            else:
+                print('Left Button Released')
+
+        if b != state_right:  # Button state changed
+            state_right = b
+            print(b)
+            if b < 0:
+                print('Right Button Pressed')
+            else:
+                print('Right Button Released')
+        time.sleep(0.001)
+
+        # time.sleep(1)
 
 
 if __name__ == '__main__':
